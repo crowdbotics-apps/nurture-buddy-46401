aaa0e51bd59c557c89ebb0a97e3feee86ff1dbd8
Crowdbotics <team@crowdbotics.com>
2024-02-16 16:33:03 +0000

    Initial commit

---
+++ backend/nurture_buddy_46401/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for nurture_buddy_46401 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "nurture_buddy_46401.settings"
+)
+
+application = get_wsgi_application()
